f7da81a86dce110358ad3e07ee6a28bca9de4bf7
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2023-02-22 23:30:18 -0300

    inserindo classe legumes e inserindo o implements nas classes

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class Alimento {
   double peso;
 
   //construtor
-  Alimento(this.nome, this.cor, this.peso);
+  Alimento({required this.nome, required this.cor, required this.peso});
 
   //metodo
   void exibir() {
@@ -27,17 +27,18 @@ class Fruta extends Alimento {
     required double peso,
     required this.diasDesdeColhieta,
     this.estaMadura,
-  }) : super(nome, cor,
-            peso); //super quer dizer que pegamos o atributo da classe alimento
+  }) : super(
+          nome: nome,
+          cor: cor,
+          peso: peso,
+        ); //super quer dizer que pegamos o atributo da classe alimento
 
   void maduro(int diasparamadurar) {
-    estaMadura = (diasDesdeColhieta <= diasparamadurar)
-        ? "sim esta madura,"
-        : "não esta madura,";
+    estaMadura = (diasDesdeColhieta <= diasparamadurar) ? "sim" : "não";
   }
 }
 
-class FrutaCitrica extends Fruta {
+class FrutaCitrica extends Fruta implements Suco {
   int nivelcitrico;
 
   FrutaCitrica(
@@ -61,6 +62,25 @@ class FrutaCitrica extends Fruta {
     }
     print("até que ta suave: nivel citrico é $nivelcitrico");
   }
+
+  @override
+  colher() {
+    if (estaMadura == "sim") {
+      print("podemos colher esta $nome");
+      return;
+    }
+    print("pegue outra $nome, essa não está boa para fazer suco");
+  }
+
+  @override
+  preparar() {
+    print("descascar $nome, bater no liquidificador com agua e colocar açucar");
+  }
+
+  @override
+  servir() {
+    print("colocar no copo e servir o suco de $nome");
+  }
 }
 
 class FrutaDoce extends Fruta {
@@ -78,11 +98,30 @@ class FrutaDoce extends Fruta {
             peso: peso,
             diasDesdeColhieta: diasDesdeColheita,
             estaMadura: estaMadura);
+  void doce() {
+    if (nivelDoce >= 6) {
+      print(" docinha");
+      return;
+    }
+    print("não ta doce não");
+    return;
+  }
+}
+
+class Legumes extends Alimento {
+  bool isPrecisaCozinhar;
+
+  Legumes({
+    required String nome,
+    required String cor,
+    required double peso,
+    required this.isPrecisaCozinhar,
+  }) : super(nome: nome, cor: cor, peso: peso);
 }
 
 //a classe abstrata podemos inserir ela em todas as classe sendo que em cada classe ela vai fazer uma coisa diferente.
 abstract class Suco {
-  colheita();
+  colher();
   preparar();
   servir();
 }
